af79ec846506cf54fb3c8e70edc25d5b49749b92
Rob Dunning <rob@raspberryridgesheepfarm.com>
2012-02-28 18:54:09 -0500

    FeedNMEASerial.cpp: support a sync'd Flarm port
    Add a second serial port to simulate a Flarm device.
    Both devices are driven by the same NMEA input file but
    the non-Flarm port does not receive the Flarm NMEA
    sentences.
    Allows simulating two serial ports in real-time with
    synchronized output.

--- test/src/FeedNMEASerial.cpp
+++ test/src/FeedNMEASerial.cpp
@@ -22,10 +22,14 @@ Copyright_License {
 */
 
 /*
- * This program creates a serial port and feeds an
- * NMEA data read from stdin to it.
- * It can be used to output to a physical serial port to which a device
- * running XCSoar is attached.
+ * This program creates one or two serial ports: one for normal NMEA,
+ * and optionally one for FLARM data.
+ * The input is a single FLARM NMEA file from stdin.
+ * It feeds the NMEA contents stripped of the Flarm records to the first
+ * serial port.
+ * It feeds the entire NMEA file to the optional second serial port.
+ * It can be used to simulate two serial inputs to XCSoar, one for smart vario
+ * data and one for Flarm data.
  */
 
 #include "OS/PathName.hpp"
@@ -45,22 +49,41 @@ Copyright_License {
 
 int main(int argc, char **argv)
 {
-  if (argc != 3) {
-    fprintf(stderr, "Usage: %s PORT BAUD < 'nmea file name'\n", argv[0]);
+  if (argc != 5 && argc != 3) {
+    fprintf(stderr, "Usage: %s PORT BAUD [PORT_FLARM] [BAUD_FLARM] < 'nmea file name'\n", argv[0]);
     return EXIT_FAILURE;
   }
 
+  const bool sync_flarm = argc == 5 ? true : false;
+
   PathName port_name(argv[1]);
   int baud = atoi(argv[2]);
+  PathName port_name_flarm(sync_flarm ? argv[3] : "");
+  int baud_flarm = sync_flarm ? atoi(argv[4]) : 0;
+
 
 #ifdef HAVE_POSIX
   TTYPort *port = new TTYPort(port_name, baud, *(Port::Handler *)NULL);
+  TTYPort *port_flarm = NULL;
+  if (sync_flarm)
+    port_flarm = new TTYPort(port_name_flarm, baud_flarm,
+                             *(Port::Handler *)NULL);
 #else
   SerialPort *port = new SerialPort(port_name, baud, *(Port::Handler *)NULL);
+  SerialPort *port_flarm = NULL;
+  if (sync_flarm)
+    port_flarm = new SerialPort(port_name_flarm, baud_flarm,
+                                *(Port::Handler *)NULL);
 #endif
   if (!port->Open()) {
     delete port;
-    fprintf(stderr, "Failed to open COM port\n");
+    fprintf(stderr, "Failed to open first COM port\n");
+    return EXIT_FAILURE;
+  }
+
+  if (sync_flarm && !port_flarm->Open()) {
+    delete port_flarm;
+    fprintf(stderr, "Failed to open Flarm COM port\n");
     return EXIT_FAILURE;
   }
 
@@ -83,14 +106,33 @@ int main(int argc, char **argv)
     }
 
     size_t length = strlen(line);
-    ssize_t nbytes = port->Write(line, length);
-    if (nbytes < 0) {
-      perror("Failed to write to port\n");
-      delete port;
-      return EXIT_FAILURE;
+    // only send non-flarm sentences to the main device
+    if (memcmp(line, "$GP", 3) == 0 &&
+        memcmp(line + 3, "FLAA", 4) != 0 &&
+         memcmp(line + 3, "FLAU", 4) != 0) {
+      ssize_t nbytes = port->Write(line, length);
+      if (nbytes < 0) {
+        perror("Failed to write to port\n");
+        delete port;
+        delete port_flarm;
+        return EXIT_FAILURE;
+      }
+    }
+
+    // send all sentences to the flarm port
+    if (sync_flarm) {
+      ssize_t nbytes_flarm = port_flarm->Write(line, length);
+      if (nbytes_flarm < 0) {
+        perror("Failed to write to Flarm port\n");
+        delete port;
+        delete port_flarm;
+        return EXIT_FAILURE;
+      }
     }
   }
 
   delete port;
+  if (sync_flarm)
+    delete port_flarm;
   return EXIT_SUCCESS;
 }
