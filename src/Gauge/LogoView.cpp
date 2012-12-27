11723d5556a313035e45b30206b14e7bc92e526d
Rob Dunning <rob@raspberryridgesheepfarm.com>
2012-12-27 13:01:27 -0500

    options.mk: always compile with NO_HORIZON=y
    and dont' display it on the logo screen

--- src/Gauge/LogoView.cpp
+++ src/Gauge/LogoView.cpp
@@ -111,7 +111,4 @@ LogoView::draw(Canvas &canvas, const PixelRect &rc)
   canvas.SetBackgroundTransparent();
   canvas.text(2, 2, TopHat_ProductToken);
   canvas.text(2, 4 + text_height, XCSoar_ProductToken);
-#ifdef NO_HORIZON
-  canvas.text(2, 6 + text_height * 2, _T("Horizon: disabled"));
-#endif
 }
