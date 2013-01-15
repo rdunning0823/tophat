47a74fec19c0a14715da3a1aac24055992239759
Rob Dunning <rob@raspberryridgesheepfarm.com>
2013-01-15 16:22:06 -0500

    ExpandMacros.cpp: change On/Off to Show/Hide for terrain etc
    change to "Show/Hide" Terrain, Show/Hide Topo,
    Show/Hide airspace

--- src/Menu/ExpandMacros.cpp
+++ src/Menu/ExpandMacros.cpp
@@ -519,16 +519,16 @@ ButtonLabel::ExpandMacros(const TCHAR *In, TCHAR *OutBuffer, size_t Size)
 		                  _("Manual"), _("Auto"), Size);
   CondReplaceInString(GetMapSettings().topography_enabled, OutBuffer,
                       _T("$(TopologyToggleActionName)"),
-                      _("Off"), _("On"), Size);
+                      _("Hide"), _("Show"), Size);
   CondReplaceInString(GetMapSettings().topography_enabled, OutBuffer,
                       _T("$(TopographyToggleActionName)"),
-                      _("Off"), _("On"), Size);
+                      _("Hide"), _("Show"), Size);
   CondReplaceInString(GetMapSettings().terrain.enable, OutBuffer,
                       _T("$(TerrainToggleActionName)"),
-                      _("Off"), _("On"), Size);
+                      _("Hide"), _("Show"), Size);
   CondReplaceInString(GetMapSettings().airspace.enable, OutBuffer,
                       _T("$(AirspaceToggleActionName)"),
-                      _("Off"), _("On"), Size);
+                      _("Hide"), _("Show"), Size);
 
   if (_tcsstr(OutBuffer, _T("$(MapLabelsToggleActionName)"))) {
     static const TCHAR *const labels[] = { N_("All"),
