e07cba7cb0d3559e5fa4e760f16ff10832825267
Rob Dunning <rob@raspberryridgesheepfarm.com>
2013-03-20 12:38:05 -0400

    GlueMapWindowItems.cpp: don's show OZs in popup if no target

--- src/MapWindow/GlueMapWindowItems.cpp
+++ src/MapWindow/GlueMapWindowItems.cpp
@@ -89,6 +89,11 @@ GlueMapWindow::ShowMapItems(const GeoPoint &location,
 
   MapItemList list;
   MapItemListBuilder builder(list, location, range);
+  bool has_targets = false;
+  if (protected_task_manager != nullptr) {
+    ProtectedTaskManager::Lease task_manager(*protected_task_manager);
+    has_targets = task_manager->GetOrderedTask().HasTargets();
+  }
 
   if (settings.item_list.add_location)
       builder.AddLocation(basic, terrain);
@@ -100,7 +105,7 @@ GlueMapWindow::ShowMapItems(const GeoPoint &location,
   if (basic.location_available)
     builder.AddSelfIfNear(basic.location, calculated.heading);
 
-  if (task)
+  if (task && has_targets)
     builder.AddTaskOZs(*task);
 
   const Airspaces *airspace_database = airspace_renderer.GetAirspaces();
