55322dc8c32629bfbf9898e639d035167ca97a02
Rob Dunning <rob@raspberryridgesheepfarm.com>
2013-03-20 09:45:09 -0400

    WaypointListSimple.cpp: reset name filter on dialog open

--- src/Dialogs/WaypointListSimple.cpp
+++ src/Dialogs/WaypointListSimple.cpp
@@ -421,6 +421,8 @@ ShowWaypointListDialogSimple(SingleWindow &parent, const GeoPoint &_location,
   assert(name_sort_frame != nullptr);
   assert(distance_sort_frame != nullptr);
 
+  dialog_state.name = _T("");
+
   PrepareButtons();
 
   location = _location;
