5323d9355ed80596d6eeaa6aad55bf30f1926e77
Rob Dunning <rob@raspberryridgesheepfarm.com>
2013-04-17 09:04:44 -0400

    XCSoar.cpp: fix log text bug
    was repeating "based on based on" in "starting top hat" message.

--- src/Android/Main.cpp
+++ src/Android/Main.cpp
@@ -121,7 +121,7 @@ Java_org_tophat_NativeView_initializeNative(JNIEnv *env, jobject obj,
 
   InitialiseDataPath();
 
-  LogStartUp(_T("Starting %s based on %s"), TopHat_ProductToken, XCSoar_ProductToken);
+  LogStartUp(_T("Starting %s %s"), TopHat_ProductToken, XCSoar_ProductToken);
 
   OpenGL::Initialise();
   TextUtil::Initialise(env);
