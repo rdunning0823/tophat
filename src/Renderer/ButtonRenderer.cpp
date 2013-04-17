a6962d16b29c5de3721221c7783c26be93c23e07
Rob Dunning <rob@raspberryridgesheepfarm.com>
2013-04-17 09:04:15 -0400

    Screen/Util.cpp: refactor - consolidate Fancy button code
    Add DrawButtonFancy, which is called by both Canvas.cpp and
    by ButtonRenderer.cpp for OPENGL

--- src/Renderer/ButtonRenderer.cpp
+++ src/Renderer/ButtonRenderer.cpp
@@ -26,57 +26,9 @@ Copyright_License {
 #include "Screen/Canvas.hpp"
 #include "Screen/Pen.hpp"
 #include "Look/ButtonLook.hpp"
+#include "Screen/Util.hpp"
 
 #ifdef ENABLE_OPENGL
-static void
-BuildButtonShape(RasterPoint *points, const PixelRect &rc)
-{
-  unsigned scale = 1; //(rc.bottom - rc.top > 30) ? 4 : 1;
-
-  //left to left top
-  points[0].x = rc.left;
-  points[0].y = rc.top + 4 * scale;
-  points[1].x = rc.left + 1 * scale;
-  points[1].y = rc.top + 2 * scale;
-
-  points[2].x = rc.left + 2 * scale;
-  points[2].y = rc.top + 1 * scale;
-  points[3].x = rc.left + 4 * scale;
-  points[3].y = rc.top;
-
-  //top to top right
-  points[4].x = rc.right - 4 * scale;
-  points[4].y = points[3].y;
-  points[5].x = rc.right - 2 * scale;
-  points[5].y = points[2].y;
-
-  points[6].x = rc.right - 1 * scale;
-  points[6].y = points[1].y;
-  points[7].x = rc.right;
-  points[7].y = points[0].y;
-
-  //right to right bottom
-  points[8].x = points[7].x;
-  points[8].y = rc.bottom - 4 * scale;
-  points[9].x = points[6].x;
-  points[9].y = rc.bottom - 2 * scale;
-
-  points[10].x = points[5].x;
-  points[10].y = rc.bottom - 1 * scale;
-  points[11].x = points[4].x;
-  points[11].y = rc.bottom;
-
-  //bottom to bottom left
-  points[12].x = points[3].x;
-  points[12].y = points[11].y;
-  points[13].x = points[2].x;
-  points[13].y = points[10].y;
-
-  points[14].x = points[1].x;
-  points[14].y = points[9].y;
-  points[15].x = points[0].x;
-  points[15].y = points[8].y;
-}
 
 void
 ButtonRenderer::DrawButton(Canvas &canvas, PixelRect rc, bool focused,
@@ -84,23 +36,8 @@ ButtonRenderer::DrawButton(Canvas &canvas, PixelRect rc, bool focused,
 {
   const ButtonLook::StateLook &_look = focused ? look.focused : look.standard;
 
-  canvas.Select(pressed ? _look.light_border_pen : _look.dark_border_pen);
-  RasterPoint points[16];
-  rc.right -= 1;
-
-   if (!transparent) {
-     PixelRect rc_inner = rc;
-     GrowRect(rc_inner, -1, -1);
-     canvas.DrawFilledRectangle(rc_inner, _look.background_color);
-   }
-
-  canvas.SelectHollowBrush();
-  BuildButtonShape(points, rc);
-  canvas.DrawPolygon(points, 16);
-
-  GrowRect(rc, -1, -1);
-  BuildButtonShape(points, rc);
-  canvas.DrawPolygon(points, 16);
+  DrawButtonFancy(canvas, rc, _look.dark_border_pen, _look.light_border_pen,
+                  _look.background_color, focused, pressed, transparent);
 }
 
 #else
