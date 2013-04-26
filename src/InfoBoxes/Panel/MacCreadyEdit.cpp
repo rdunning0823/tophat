3ffda990347db67d4cd80dcede07b21e404f9813
Rob Dunning <rob@raspberryridgesheepfarm.com>
2013-04-25 23:23:59 -0400

    Panel/MacCready: add final glide bar
    The visualization of the final glide bar allows users
    to adjust their MC setting to with respect to their final glide
    altitude.  So, we display it on the widget.
    
    Conflicts:
    	src/InfoBoxes/Panel/MacCreadyEdit.cpp

--- src/InfoBoxes/Panel/MacCreadyEdit.cpp
+++ src/InfoBoxes/Panel/MacCreadyEdit.cpp
@@ -29,16 +29,17 @@ Copyright_License {
 #include "Form/SymbolButton.hpp"
 #include "Form/CheckBox.hpp"
 #include "Form/Frame.hpp"
-#include "Form/Panel.hpp"
+#include "Screen/Timer.hpp"
 #include "Look/Fonts.hpp"
 #include "Formatter/UserUnits.hpp"
 #include "Units/Units.hpp"
 #include "UIGlobals.hpp"
 #include "Interface.hpp"
 #include "Screen/Layout.hpp"
-#include "Screen/PaintWindow.hpp"
+#include "Util/StaticString.hpp"
 #include "Renderer/FinalGlideBarRenderer.hpp"
 #include "Look/Look.hpp"
+#include "Screen/Canvas.hpp"
 #include "Profile/Profile.hpp"
 #include "ActionInterface.hpp"
 
@@ -50,9 +51,32 @@ enum ControlIndex {
   AutoMc,
 };
 
-class MacCreadyEditPanel : public BaseAccessPanel, NumberButtonLayout {
-protected:
 
+class MacCreadyEditPanel : public BaseAccessPanel, NumberButtonLayout {
+  class FinalGlideChart: public PaintWindow
+  {
+  public:
+    FinalGlideChart(ContainerWindow &parent,
+                    PixelScalar x, PixelScalar y,
+                    UPixelScalar Width, UPixelScalar Height,
+                    WindowStyle style,
+                    const Look&);
+  protected:
+    const Look& look;
+
+    /**
+     * draws the Final Glide Bar on the MC widget so the pilot can
+     * adjust his MC with respect to the arrival height
+     */
+    FinalGlideBarRenderer *final_glide_bar_renderer;
+
+    /**
+     * draws the the Final Glide bar
+     */
+    virtual void OnPaint(Canvas &canvas);
+  };
+
+protected:
   /**
    * These 4 buttons and the mc_value frame use the layout rectangles
    * calculated in NumberButtonLayout
@@ -61,9 +85,26 @@ protected:
   WndFrame *mc_value;
   CheckBoxControl *auto_mc;
 
+  /**
+   * draws the Final Glide Bar on the MC widget so the pilot can
+   * adjust his MC with respect to the arrival height
+   */
+  FinalGlideChart *final_glide_chart;
+
+  /**
+   * Area where canvas will draw the final glide bar
+   */
+  PixelRect fg_rc;
+
+  /**
+   * This timer updates the data for the final glide
+   */
+  WindowTimer dialog_timer;
+
+
 public:
   MacCreadyEditPanel(unsigned _id)
-    :BaseAccessPanel(_id) {}
+    :BaseAccessPanel(_id), dialog_timer(*this) {}
 
   virtual void Prepare(ContainerWindow &parent, const PixelRect &rc);
   virtual void Unprepare();
@@ -71,9 +112,28 @@ public:
   void Refresh();
 
 protected:
+  /**
+   * render the final glide periodically because
+   * latency in the blackboards causes the final glide
+   * renderer to not always use value updated with the
+   * buttons
+   */
+  virtual bool OnTimer(WindowTimer &timer);
+
   /* methods from ActionListener */
   virtual void OnAction(int id);
 };
+
+
+bool
+MacCreadyEditPanel::OnTimer(WindowTimer &timer)
+{
+  if (timer == dialog_timer) {
+    Refresh();
+    return true;
+  }
+  return BaseAccessPanel::OnTimer(timer);
+}
 
 void
 MacCreadyEditPanel::OnAction(int action_id)
@@ -125,11 +185,12 @@ MacCreadyEditPanel::Refresh()
   const ComputerSettings &settings_computer =
     CommonInterface::GetComputerSettings();
   auto_mc->SetState(XCSoarInterface::GetComputerSettings().task.auto_mc);
-  TCHAR buffer[32];
-  fixed value = settings_computer.polar.glide_polar_task.GetMC();
-  FormatUserVerticalSpeed(value, buffer, false);
-  mc_value->SetCaption(buffer);
+  fixed mc = settings_computer.polar.glide_polar_task.GetMC();
+  StaticString<32> buffer;
+  FormatUserVerticalSpeed(mc, buffer.buffer(), false);
+  mc_value->SetCaption(buffer.c_str());
   mc_value->SetEnabled(!auto_mc->GetState());
+  final_glide_chart->Invalidate();
 }
 
 void
@@ -138,36 +199,55 @@ MacCreadyEditPanel::Prepare(ContainerWindow &parent, const PixelRect &rc)
   BaseAccessPanel::Prepare(parent, rc);
   NumberButtonLayout::Prepare(parent, content_rc);
 
-  const DialogLook &look = UIGlobals::GetDialogLook();
+  PixelRect content_right_rc = content_rc;
+  PixelRect content_left_rc = content_rc;
 
+  // split content area into two columns, buttons on the right, fg on left
+  content_right_rc.left += Layout::Scale(50);
+  content_left_rc.right = content_right_rc.left - 1;
+
+  NumberButtonLayout::Prepare(parent, content_right_rc);
+
+  WindowStyle style;
+  const Look &look = UIGlobals::GetLook();
+  fg_rc = content_left_rc;
+
+  final_glide_chart =
+      new FinalGlideChart(GetClientAreaWindow(),
+                          fg_rc.left, fg_rc.top,
+                          (UPixelScalar)(fg_rc.right - fg_rc.left),
+                          (UPixelScalar)(fg_rc.bottom - fg_rc.top),
+                          style, look);
+
+  const DialogLook &dialog_look = UIGlobals::GetDialogLook();
   ButtonWindowStyle button_style;
   button_style.TabStop();
   button_style.multiline();
-  big_plus = new WndSymbolButton(GetClientAreaWindow(), look, _T("^"),
+  big_plus = new WndSymbolButton(GetClientAreaWindow(), dialog_look, _T("^"),
                                  big_plus_rc,
                                  button_style, *this, BigPlus);
   big_plus->SetFont(Fonts::infobox);
 
-  little_plus = new WndSymbolButton(GetClientAreaWindow(), look, _T("^"),
-                                    little_plus_rc,
+  little_plus = new WndSymbolButton(GetClientAreaWindow(), dialog_look,
+                                    _T("^"), little_plus_rc,
                                     button_style, *this, LittlePlus);
   little_plus->SetFont(Fonts::infobox);
 
-  big_minus = new WndSymbolButton(GetClientAreaWindow(), look, _T("v"),
-                                  big_minus_rc,
+  big_minus = new WndSymbolButton(GetClientAreaWindow(), dialog_look,
+                                  _T("v"), big_minus_rc,
                                   button_style, *this, BigMinus);
   big_minus->SetFont(Fonts::infobox);
 
-  little_minus = new WndSymbolButton(GetClientAreaWindow(), look, _T("v"),
-                                     little_minus_rc,
+  little_minus = new WndSymbolButton(GetClientAreaWindow(), dialog_look,
+                                     _T("v"), little_minus_rc,
                                      button_style, *this, LittleMinus);
   little_minus->SetFont(Fonts::infobox);
 
   WindowStyle style_frame;
-  mc_value = new WndFrame(GetClientAreaWindow(), look,
+  mc_value = new WndFrame(GetClientAreaWindow(), dialog_look,
                           value_rc, style_frame);
-  mc_value->SetVAlignCenter();
   mc_value->SetAlignCenter();
+  mc_value->SetVAlignCenter();
   mc_value->SetFont(Fonts::infobox);
 
   PixelRect checkbox_rc;
@@ -176,14 +256,15 @@ MacCreadyEditPanel::Prepare(ContainerWindow &parent, const PixelRect &rc)
   checkbox_rc.top = big_minus_rc.bottom +
     (content_rc.bottom - big_minus_rc.bottom) / 4;
   checkbox_rc.left = big_minus_rc.left;
-  checkbox_rc.right = big_minus_rc.right;
+  checkbox_rc.right = little_minus_rc.right;
 
   ButtonWindowStyle checkbox_style;
   checkbox_style.TabStop();
 
-  auto_mc = new CheckBoxControl(GetClientAreaWindow(), look, _T("Auto MC"),
+  auto_mc = new CheckBoxControl(GetClientAreaWindow(), dialog_look, _T("Auto MC"),
                                 checkbox_rc, checkbox_style,
                                 this, AutoMc);
+  dialog_timer.Schedule(500);
   Refresh();
 }
 
@@ -196,6 +277,8 @@ MacCreadyEditPanel::Unprepare()
   delete little_minus;
   delete mc_value;
   delete auto_mc;
+  delete final_glide_chart;
+  dialog_timer.Cancel();
   BaseAccessPanel::Unprepare();
 }
 
@@ -203,4 +286,33 @@ Widget *
 LoadMacCreadyEditPanel(unsigned id)
 {
   return new MacCreadyEditPanel(id);
+}
+
+
+MacCreadyEditPanel::FinalGlideChart::FinalGlideChart(ContainerWindow &parent,
+                                                     PixelScalar X,
+                                                     PixelScalar Y,
+                                                     UPixelScalar width,
+                                                     UPixelScalar height,
+                                                     WindowStyle style,
+                                                     const Look& _look)
+  :look(_look)
+{
+  PixelRect rc (X, Y, X + width, Y + height);
+  Create(parent, rc, style);
+  final_glide_bar_renderer = new FinalGlideBarRenderer(look.final_glide_bar,
+                                                       look.map.task);
+}
+
+void
+MacCreadyEditPanel::FinalGlideChart::OnPaint(Canvas &canvas)
+{
+  PaintWindow::OnPaint(canvas);
+  canvas.SelectNullPen();
+  canvas.Clear(look.dialog.background_color);
+
+  final_glide_bar_renderer->Draw(canvas, GetClientRect(),
+                                 CommonInterface::Calculated(),
+                                 CommonInterface::GetComputerSettings().task.glide,
+    CommonInterface::GetUISettings().map.final_glide_bar_mc0_enabled);
 }
