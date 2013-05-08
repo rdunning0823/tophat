d33a4c1e5f83fe93ee386c55035e577c78cd8353
Max Kellermann <max@duempel.org>
2013-05-08 10:21:52 +0200

    PageActions: don't forget map zoom when returning to map
    Call LeavePage() in PageActions::OpenLayout(), to remember the current
    map zoom before switching to a "special" page.  Fixes TRAC #2805.

--- src/PageActions.cpp
+++ src/PageActions.cpp
@@ -246,6 +246,8 @@ PageActions::LoadLayout(const PageLayout &layout)
 void
 PageActions::OpenLayout(const PageLayout &layout)
 {
+  LeavePage();
+
   PagesState &state = CommonInterface::SetUIState().pages;
   state.special_page = layout;
 
