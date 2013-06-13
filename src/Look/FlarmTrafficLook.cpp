fd47a279cdca1f8d38d151fa1b4f5522d70bc426
Rob Dunning <rob@raspberryridgesheepfarm.com>
2013-06-12 23:18:53 -0400

    FlarmTrafficLook.cpp: If Grayscale screen, use clearer colors

--- src/Look/FlarmTrafficLook.cpp
+++ src/Look/FlarmTrafficLook.cpp
@@ -35,7 +35,8 @@ FlarmTrafficLook::Initialise(const TrafficLook &other, bool small, bool inverse)
   default_color = inverse ? COLOR_WHITE : COLOR_BLACK;
   selection_color = COLOR_BLUE;
   background_color = inverse ? COLOR_BLACK : COLOR_WHITE;
-  radar_color = COLOR_GRAY;
+  radar_color = inverse ? COLOR_WHITE : COLOR_BLACK;
+
   Color team_color_green = Color(0x74, 0xFF, 0);
   Color team_color_blue = Color(0, 0x90, 0xFF);
   Color team_color_yellow = Color(0xFF, 0xE8, 0);
