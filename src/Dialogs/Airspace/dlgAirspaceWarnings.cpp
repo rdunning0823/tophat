XCSoar/XCSoar

Dialogs/Airspace/dlgAirspaceWarnings: Don't restrict airspace name to first 20 characters

src/Dialogs/Airspace/dlgAirspaceWarnings.cpp
@@ -39,6 +39,7 @@ Copyright_License {
 #include "Formatter/AirspaceFormatter.hpp"
 #include "Engine/Airspace/AbstractAirspace.hpp"
 #include "Util/TrivialArray.hpp"
+#include "Util/Macros.hpp"
 #include "Interface.hpp"
 #include "Language/Language.hpp"
 #include "Event/LambdaTimer.hpp"
@@ -361,7 +362,7 @@ AirspaceWarningListHandler::OnPaintItem(Canvas &canvas,
     canvas.SetTextColor(COLOR_GRAY);
 
   { // name, altitude info
-    _sntprintf(buffer, 21, _T("%s %s"),
+    _sntprintf(buffer, ARRAY_SIZE(buffer), _T("%s %s"),
                airspace.GetName(),
                AirspaceFormatter::GetClass(airspace));
 

rdunning0823/tophat

src/Dialogs/Airspace/dlgAirspaceWarnings.cpp
@@ -39,6 +39,7 @@ Copyright_License {
 #include "Formatter/AirspaceFormatter.hpp"
 #include "Engine/Airspace/AbstractAirspace.hpp"
 #include "Util/TrivialArray.hpp"
+#include "Util/Macros.hpp"
 #include "Interface.hpp"
 #include "Language/Language.hpp"
 #include "Event/LambdaTimer.hpp"
@@ -361,7 +362,7 @@ AirspaceWarningListHandler::OnPaintItem(Canvas &canvas,
     canvas.SetTextColor(COLOR_GRAY);
 
   { // name, altitude info
-    StringFormat(buffer, 21, _T("%s %s"),
+    StringFormat(buffer, ARRAY_SIZE(buffer), _T("%s %s"),
                  airspace.GetName(),
                  AirspaceFormatter::GetClass(airspace));
 