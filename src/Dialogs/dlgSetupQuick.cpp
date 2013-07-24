2823565b28c50dde044c54882ac65b94bec0805c
Rob Dunning <rob@raspberryridgesheepfarm.com>
2013-07-23 20:30:51 -0400

    dlgSetupQuick.cpp: fix format of device display string

--- src/Dialogs/dlgSetupQuick.cpp
+++ src/Dialogs/dlgSetupQuick.cpp
@@ -276,7 +276,7 @@ SetupQuick::RefreshForm()
   if (config.UsesDriver()) {
     const TCHAR *driver_name = FindDriverDisplayName(config.driver_name);
 
-    text.AppendFormat(_("%s on %s"), driver_name, port_name);
+    text.AppendFormat(_("%s on %s, "), driver_name, port_name);
     text.append(driver_name);
   } else {
     text.append(port_name);
