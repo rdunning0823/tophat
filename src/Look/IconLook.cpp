0915dd5d7d50283baa187113779f4529583f8fb3
Rob Dunning <rob@raspberryridgesheepfarm.com>
2013-11-22 19:58:21 -0500

    IconLook.cpp: use HD2 icons for hires screens

--- src/Look/IconLook.cpp
+++ src/Look/IconLook.cpp
@@ -28,30 +28,31 @@ Copyright_License {
 void
 IconLook::Initialise()
 {
-  hBmpTabTask.Load(Layout::scale > 1 ? IDB_TASK_HD : IDB_TASK);
-  hBmpTabWrench.Load(Layout::scale > 1 ? IDB_WRENCH_HD : IDB_WRENCH);
-  hBmpTabSettings.Load(Layout::scale > 1 ? IDB_SETTINGS_HD : IDB_SETTINGS);
-  hBmpTabCalculator.Load(Layout::scale > 1 ? IDB_CALCULATOR_HD : IDB_CALCULATOR);
+  int s = Layout::scale;
+  hBmpTabTask.Load(GET_ICON(s, IDB_TASK_HD2 ,IDB_TASK_HD, IDB_TASK));
+  hBmpTabWrench.Load(GET_ICON(s, IDB_WRENCH_HD2 ,IDB_WRENCH_HD, IDB_WRENCH));
+  hBmpTabSettings.Load(GET_ICON(s, IDB_SETTINGS_HD2 ,IDB_SETTINGS_HD, IDB_SETTINGS));
+  hBmpTabCalculator.Load(GET_ICON(s, IDB_CALCULATOR_HD ,IDB_CALCULATOR_HD, IDB_CALCULATOR));
 
-  hBmpTabFlight.Load(Layout::scale > 1 ? IDB_GLOBE_HD : IDB_GLOBE);
-  hBmpTabSystem.Load(Layout::scale > 1 ? IDB_DEVICE_HD : IDB_DEVICE);
-  hBmpTabRules.Load(Layout::scale > 1 ? IDB_RULES_HD : IDB_RULES);
-  hBmpTabTimes.Load(Layout::scale > 1 ? IDB_CLOCK_HD : IDB_CLOCK);
+  hBmpTabFlight.Load(GET_ICON(s, IDB_GLOBE_HD2 ,IDB_GLOBE_HD, IDB_GLOBE));
+  hBmpTabSystem.Load(GET_ICON(s, IDB_DEVICE_HD2 ,IDB_DEVICE_HD, IDB_DEVICE));
+  hBmpTabRules.Load(GET_ICON(s, IDB_RULES_HD2 ,IDB_RULES_HD, IDB_RULES));
+  hBmpTabTimes.Load(GET_ICON(s, IDB_CLOCK_HD2 ,IDB_CLOCK_HD, IDB_CLOCK));
 
 
-  hBmpScreensButton.Load(Layout::scale > 1 ? IDB_SCREENS_BUTTON_HD : IDB_SCREENS_BUTTON);
-  hBmpMenuButton.Load(Layout::scale > 1 ? IDB_MENU_BUTTON_HD : IDB_MENU_BUTTON);
-  hBmpCheckMark.Load(Layout::scale > 1 ? IDB_CHECK_MARK_HD : IDB_CHECK_MARK);
-  hBmpSearch.Load(Layout::scale > 1 ? IDB_SEARCH_HD : IDB_SEARCH);
-  hBmpSearchChecked.Load(Layout::scale > 1 ? IDB_SEARCH_CHECKED_HD : IDB_SEARCH_CHECKED);
-  hBmpZoomOutButton.Load(Layout::scale > 1 ? IDB_ZOOM_OUT_BUTTON_HD : IDB_ZOOM_OUT_BUTTON);
-  hBmpZoomInButton.Load(Layout::scale > 1 ? IDB_ZOOM_IN_BUTTON_HD : IDB_ZOOM_IN_BUTTON);
-  hBmpBearingLeftOne.Load(Layout::scale > 1 ? IDB_BEARING_LEFT_ONE_HD : IDB_BEARING_LEFT_ONE);
-  hBmpBearingLeftTwo.Load(Layout::scale > 1 ? IDB_BEARING_LEFT_TWO_HD : IDB_BEARING_LEFT_TWO);
-  hBmpBearingLeftThree.Load(Layout::scale > 1 ? IDB_BEARING_LEFT_THREE_HD : IDB_BEARING_LEFT_THREE);
-  hBmpBearingLeftFour.Load(Layout::scale > 1 ? IDB_BEARING_LEFT_FOUR_HD : IDB_BEARING_LEFT_FOUR);
-  hBmpBearingRightOne.Load(Layout::scale > 1 ? IDB_BEARING_RIGHT_ONE_HD : IDB_BEARING_RIGHT_ONE);
-  hBmpBearingRightTwo.Load(Layout::scale > 1 ? IDB_BEARING_RIGHT_TWO_HD : IDB_BEARING_RIGHT_TWO);
-  hBmpBearingRightThree.Load(Layout::scale > 1 ? IDB_BEARING_RIGHT_THREE_HD : IDB_BEARING_RIGHT_THREE);
-  hBmpBearingRightFour.Load(Layout::scale > 1 ? IDB_BEARING_RIGHT_FOUR_HD : IDB_BEARING_RIGHT_FOUR);
+  hBmpScreensButton.Load(GET_ICON(s, IDB_SCREENS_BUTTON_HD2 ,IDB_SCREENS_BUTTON_HD, IDB_SCREENS_BUTTON));
+  hBmpMenuButton.Load(GET_ICON(s, IDB_MENU_BUTTON_HD2 ,IDB_MENU_BUTTON_HD, IDB_MENU_BUTTON));
+  hBmpCheckMark.Load(GET_ICON(s, IDB_CHECK_MARK_HD2 ,IDB_CHECK_MARK_HD, IDB_CHECK_MARK));
+  hBmpSearch.Load(GET_ICON(s, IDB_SEARCH_HD2, IDB_SEARCH_HD, IDB_SEARCH));
+  hBmpSearchChecked.Load(GET_ICON(s, IDB_SEARCH_CHECKED_HD2 ,IDB_SEARCH_CHECKED_HD, IDB_SEARCH_CHECKED));
+  hBmpZoomOutButton.Load(GET_ICON(s, IDB_ZOOM_OUT_BUTTON_HD2 ,IDB_ZOOM_OUT_BUTTON_HD, IDB_ZOOM_OUT_BUTTON));
+  hBmpZoomInButton.Load(GET_ICON(s, IDB_ZOOM_IN_BUTTON_HD2 ,IDB_ZOOM_IN_BUTTON_HD, IDB_ZOOM_IN_BUTTON));
+  hBmpBearingLeftOne.Load(GET_ICON(s, IDB_BEARING_LEFT_ONE_HD2 ,IDB_BEARING_LEFT_ONE_HD, IDB_BEARING_LEFT_ONE));
+  hBmpBearingLeftTwo.Load(GET_ICON(s, IDB_BEARING_LEFT_TWO_HD2 ,IDB_BEARING_LEFT_TWO_HD, IDB_BEARING_LEFT_TWO));
+  hBmpBearingLeftThree.Load(GET_ICON(s, IDB_BEARING_LEFT_THREE_HD2 ,IDB_BEARING_LEFT_THREE_HD, IDB_BEARING_LEFT_THREE));
+  hBmpBearingLeftFour.Load(GET_ICON(s, IDB_BEARING_LEFT_FOUR_HD2 ,IDB_BEARING_LEFT_FOUR_HD, IDB_BEARING_LEFT_FOUR));
+  hBmpBearingRightOne.Load(GET_ICON(s, IDB_BEARING_RIGHT_ONE_HD2 ,IDB_BEARING_RIGHT_ONE_HD, IDB_BEARING_RIGHT_ONE));
+  hBmpBearingRightTwo.Load(GET_ICON(s, IDB_BEARING_RIGHT_TWO_HD2 ,IDB_BEARING_RIGHT_TWO_HD, IDB_BEARING_RIGHT_TWO));
+  hBmpBearingRightThree.Load(GET_ICON(s, IDB_BEARING_RIGHT_THREE_HD2 ,IDB_BEARING_RIGHT_THREE_HD, IDB_BEARING_RIGHT_THREE));
+  hBmpBearingRightFour.Load(GET_ICON(s, IDB_BEARING_RIGHT_FOUR_HD2 ,IDB_BEARING_RIGHT_FOUR_HD, IDB_BEARING_RIGHT_FOUR));
 }
