a8402f0c25bc67cc58af5794013911bfa70c09a9
Rob Dunning <rob@raspberryridgesheepfarm.com>
2014-02-15 16:26:07 -0500

    TopHatMenu.hpp: set menu timeout when menu changed
    This allows the menu timer to clear the menu after 15 seconds

--- src/Menu/TophatMenu.hpp
+++ src/Menu/TophatMenu.hpp
@@ -16,22 +16,30 @@ namespace TophatMenu {
   const TCHAR menu_1[] = _T("Menu1");
   const TCHAR menu_2[] = _T("Menu2");
   const TCHAR menu_last[] = _T("MenuLast");
+
+  void ShowMenu(const TCHAR *menu_name);
+  void ShowMenu(const TCHAR *menu_name)
+  {
+    InputEvents::ShowMenu();
+    InputEvents::setMode(menu_name);
+  }
+
   void RotateMenu();
   void RotateMenu()
   {
     if (InputEvents::IsMode(menu))
-      InputEvents::setMode(menu_1);
+      ShowMenu(menu_1);
 
     else if (InputEvents::IsMode(menu_1))
-      InputEvents::setMode(menu_2);
+      ShowMenu(menu_2);
 
     else if (InputEvents::IsMode(menu_2))
-      InputEvents::setMode(menu_last);
+      ShowMenu(menu_last);
 
     else if (InputEvents::IsMode(menu_last))
       InputEvents::HideMenu();
 
-    else InputEvents::setMode(menu);
+    else ShowMenu(menu);
 
     return;
   }
