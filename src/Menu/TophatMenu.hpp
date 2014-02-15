69ea8c549dd5b757927b47df39ad465caf123102
Rob Dunning <rob@raspberryridgesheepfarm.com>
2014-02-15 16:17:25 -0500

    TopHatMenu.hpp: refactor: use TCHAR instead of StaticString

--- src/Menu/TophatMenu.hpp
+++ src/Menu/TophatMenu.hpp
@@ -9,35 +9,29 @@
 #define TOPHATMENU_HPP
 
 #include "Input/InputEvents.hpp"
-#include "Util/StaticString.hpp"
 
 namespace TophatMenu {
+
+  const TCHAR menu[] = _T("Menu");
+  const TCHAR menu_1[] = _T("Menu1");
+  const TCHAR menu_2[] = _T("Menu2");
+  const TCHAR menu_last[] = _T("MenuLast");
   void RotateMenu();
   void RotateMenu()
   {
-    StaticString<20> menu;
-    StaticString<20> menu_1;
-    StaticString<20> menu_2;
-    StaticString<20> menu_last;
+    if (InputEvents::IsMode(menu))
+      InputEvents::setMode(menu_1);
 
-    menu = _T("Menu");
-    menu_1 = _T("Menu1");
-    menu_2 = _T("Menu2");
-    menu_last = _T("MenuLast");
+    else if (InputEvents::IsMode(menu_1))
+      InputEvents::setMode(menu_2);
 
-    if (InputEvents::IsMode(menu.buffer()))
-      InputEvents::setMode(menu_1.buffer());
+    else if (InputEvents::IsMode(menu_2))
+      InputEvents::setMode(menu_last);
 
-    else if (InputEvents::IsMode(menu_1.buffer()))
-      InputEvents::setMode(menu_2.buffer());
-
-    else if (InputEvents::IsMode(menu_2.buffer()))
-      InputEvents::setMode(menu_last.buffer());
-
-    else if (InputEvents::IsMode(menu_last.buffer()))
+    else if (InputEvents::IsMode(menu_last))
       InputEvents::HideMenu();
 
-    else InputEvents::setMode(menu.buffer());
+    else InputEvents::setMode(menu);
 
     return;
   }
