6806e7e519018b04185f35b0a45b10c99ec15a41
Rob Dunning <rob@raspberryridgesheepfarm.com>
2015-09-22 23:25:18 -0400

    merge_68 fixup: SiteConfigPanel.cpp

--- src/Dialogs/Settings/Panels/SiteConfigPanel.cpp
+++ src/Dialogs/Settings/Panels/SiteConfigPanel.cpp
@@ -2,7 +2,7 @@
 Copyright_License {
 
   XCSoar Glide Computer - http://www.xcsoar.org/
-  Copyright (C) 2000-2015 The XCSoar Project
+  Copyright (C) 2000-2013 The XCSoar Project
   A detailed list of copyright holders can be found in the file "AUTHORS".
 
   This program is free software; you can redistribute it and/or
@@ -24,6 +24,12 @@ Copyright_License {
 #include "Profile/ProfileKeys.hpp"
 #include "Language/Language.hpp"
 #include "Dialogs/Dialogs.h"
+#include "Dialogs/Waypoint/WaypointDialogs.hpp"
+#include "Dialogs/FilePickAndDownload.hpp"
+#include "Form/Button.hpp"
+#include "Form/DataField/Listener.hpp"
+#include "Form/DataField/File.hpp"
+#include "Form/ActionListener.hpp"
 #include "LocalPath.hpp"
 #include "UtilsSettings.hpp"
 #include "ConfigPanel.hpp"
@@ -31,7 +37,13 @@ Copyright_License {
 #include "Widget/RowFormWidget.hpp"
 #include "UIGlobals.hpp"
 #include "Waypoint/Patterns.hpp"
-#include "Form/DataField/File.hpp"
+#include "Language/Language.hpp"
+#include "Util/StaticString.hxx"
+#include "Util/ConvertString.hpp"
+#include "Interface.hpp"
+#include "Repository/AvailableFile.hpp"
+
+#include <string>
 
 enum ControlIndex {
   DataPath,
@@ -41,22 +53,86 @@ enum ControlIndex {
   WatchedWaypointFile,
   AirspaceFile,
   AdditionalAirspaceFile,
-  AirfieldFile
+  AirfieldFile,
+  EditWaypoints,
 };
 
-class SiteConfigPanel final : public RowFormWidget {
-  enum Buttons {
-    WAYPOINT_EDITOR,
-  };
+class SiteConfigPanel final : public RowFormWidget, DataFieldListener,
+    public ActionListener {
+private:
+  Button *buttonWaypoints;
+  StaticString<50> download_name;
+  /**
+   * is this a stand-alone widget, or displayed in the dlgConfiguration menu?
+   */
+  bool stand_alone;
 
 public:
   SiteConfigPanel()
-    :RowFormWidget(UIGlobals::GetDialogLook()) {}
+  :RowFormWidget(UIGlobals::GetDialogLook()), buttonWaypoints(nullptr),
+   download_name(N_("Download from internet")) {}
+
+  SiteConfigPanel(bool _stand_alone)
+    :RowFormWidget(UIGlobals::GetDialogLook()), buttonWaypoints(nullptr),
+     download_name(N_("Download from internet")), stand_alone(_stand_alone){}
 
 public:
   virtual void Prepare(ContainerWindow &parent, const PixelRect &rc) override;
   virtual bool Save(bool &changed) override;
+
+private:
+  /* methods from DataFieldListener */
+  virtual void OnModified(DataField &df) override;
+
+  /* from ActionListener */
+  virtual void OnAction(int id) override;
 };
+
+void
+SiteConfigPanel::OnAction(int id)
+{
+  if (id == EditWaypoints)
+    dlgConfigWaypointsShowModal();
+}
+
+void
+SiteConfigPanel::OnModified(DataField &df)
+{
+  AvailableFile file_filter;
+  file_filter.Clear();
+  FileDataField* dff = (FileDataField*)&df;
+  if (IsDataField(MapFile, df))
+    file_filter.type = FileType::MAP;
+  else if (IsDataField(WaypointFile, df))
+    file_filter.type = FileType::WAYPOINT;
+  else if (IsDataField(AirspaceFile, df))
+    file_filter.type = FileType::AIRSPACE;
+  else
+    return;
+
+  StaticString<255> label;
+  label.clear();
+
+  if (dff->GetAsDisplayString() != nullptr)
+    label = dff->GetAsDisplayString();
+  if (label == dff->GetScanInternetLabel()) {
+
+    AvailableFile result = ShowFilePickAndDownload(file_filter);
+    if (result.IsValid()) {
+      ACPToWideConverter base(result.GetName());
+      if (!base.IsValid())
+        return;
+      StaticString<255> temp_name(base);
+      StaticString<255> buffer;
+      LocalPath(buffer.buffer(), base);
+      dff->AddFile(base, buffer.c_str());
+      if (buffer == dff->GetScanInternetLabel())
+        dff->SetAsInteger(0);
+      else
+        dff->Lookup(buffer.c_str());
+    }
+  }
+}
 
 void
 SiteConfigPanel::Prepare(ContainerWindow &parent, const PixelRect &rc)
@@ -65,45 +141,53 @@ SiteConfigPanel::Prepare(ContainerWindow &parent, const PixelRect &rc)
   wp->SetText(GetPrimaryDataPath());
   wp->SetEnabled(false);
 
-  AddFile(_("Map database"),
-          _("The name of the file (.xcm) containing terrain, topography, and optionally "
-            "waypoints, their details and airspaces."),
-          ProfileKeys::MapFile, _T("*.xcm\0*.lkm\0"), FileType::MAP);
+  wp = AddFile(_("Map database"),
+               _("The name of the file (.xcm) containing terrain, topography, and optionally "
+                   "waypoints, their details and airspaces."),
+               ProfileKeys::MapFile, _T("*.xcm\0*.lkm\0"), this);
+  FileDataField *dff = (FileDataField *)(wp->GetDataField());
+  dff->EnableInternetDownload();
 
-  AddFile(_("Waypoints"),
-          _("Primary waypoints file.  Supported file types are Cambridge/WinPilot files (.dat), "
-            "Zander files (.wpz) or SeeYou files (.cup)."),
-          ProfileKeys::WaypointFile, WAYPOINT_FILE_PATTERNS,
-          FileType::WAYPOINT);
+
+  wp = AddFile(_("Waypoints"),
+               _("Primary waypoints file.  Supported file types are Cambridge/WinPilot files (.dat), "
+                   "Zander files (.wpz) or SeeYou files (.cup).  Available at http://soaringweb.org/TP"),
+               ProfileKeys::WaypointFile, WAYPOINT_FILE_PATTERNS,
+               this);
+  dff = (FileDataField *)(wp->GetDataField());
+  dff->EnableInternetDownload();
 
   AddFile(_("More waypoints"),
           _("Secondary waypoints file.  This may be used to add waypoints for a competition."),
-          ProfileKeys::AdditionalWaypointFile, WAYPOINT_FILE_PATTERNS,
-          FileType::WAYPOINT);
+          ProfileKeys::AdditionalWaypointFile, WAYPOINT_FILE_PATTERNS);
   SetExpertRow(AdditionalWaypointFile);
 
   AddFile(_("Watched waypoints"),
           _("Waypoint file containing special waypoints for which additional computations like "
-            "calculation of arrival height in map display always takes place. Useful for "
-            "waypoints like known reliable thermal sources (e.g. powerplants) or mountain passes."),
-          ProfileKeys::WatchedWaypointFile, WAYPOINT_FILE_PATTERNS,
-          FileType::WAYPOINT);
+              "calculation of arrival height in map display always takes place. Useful for "
+              "waypoints like known reliable thermal sources (e.g. powerplants) or mountain passes."),
+          ProfileKeys::WatchedWaypointFile, WAYPOINT_FILE_PATTERNS);
   SetExpertRow(WatchedWaypointFile);
 
-  AddFile(_("Airspaces"), _("The file name of the primary airspace file."),
-          ProfileKeys::AirspaceFile, _T("*.txt\0*.air\0*.sua\0"),
-          FileType::AIRSPACE);
+  wp = AddFile(_("Airspaces"), _("The file name of the primary airspace file.  Available at http://soaringweb.org/TP"),
+               ProfileKeys::AirspaceFile, _T("*.txt\0*.air\0*.sua\0"),
+               this);
+  dff = (FileDataField *)(wp->GetDataField());
+  dff->EnableInternetDownload();
+
 
   AddFile(_("More airspaces"), _("The file name of the secondary airspace file."),
-          ProfileKeys::AdditionalAirspaceFile, _T("*.txt\0*.air\0*.sua\0"),
-          FileType::AIRSPACE);
+          ProfileKeys::AdditionalAirspaceFile, _T("*.txt\0*.air\0*.sua\0"));
   SetExpertRow(AdditionalAirspaceFile);
 
   AddFile(_("Waypoint details"),
           _("The file may contain extracts from enroute supplements or other contributed "
-            "information about individual waypoints and airfields."),
+              "information about individual waypoints and airfields."),
           ProfileKeys::AirfieldFile, _T("*.txt\0"));
   SetExpertRow(AirfieldFile);
+
+  if (stand_alone)
+    AddButton(_("Edit waypoints"), *this, EditWaypoints);
 }
 
 bool
@@ -132,7 +216,14 @@ SiteConfigPanel::Save(bool &_changed)
 }
 
 Widget *
+CreateSiteConfigPanel(bool stand_alone)
+{
+  return new SiteConfigPanel(stand_alone);
+}
+
+
+Widget *
 CreateSiteConfigPanel()
 {
-  return new SiteConfigPanel();
+  return CreateSiteConfigPanel(false);
 }
