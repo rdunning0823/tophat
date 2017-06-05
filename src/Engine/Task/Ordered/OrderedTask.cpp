ebc7e244d97b15c4df91fcd6776a9acb08145cad
Rob Dunning <rob@raspberryridgesheepfarm.com>
2017-06-05 16:53:07 -0400

    OrderedTask.cpp: refactor - use ? operator instead of else if

--- src/Engine/Task/Ordered/OrderedTask.cpp
+++ src/Engine/Task/Ordered/OrderedTask.cpp
@@ -1297,17 +1297,10 @@ OrderedTask::UpdateTaskMC(const GlidePolar &_glide_polar)
     if (task_points.empty() || !task_points.front()->HasEntered()) {
       stats.task_mc = glide_polar.GetMC();
 
-      // use last hour speed if flight longer than an hour
-    } else if (stats.total.time_elapsed > fixed(3600) &&
-        !negative(stats.last_hour.duration)) {
-      fixed speed = stats.last_hour.speed;
-      stats.task_mc = UpdateTaskMCIfChanged(stats.task_mc, last_task_mc_speed,
-                                            speed, glide_polar);
-      glide_polar.SetMC(stats.task_mc);
-
-      // use overall speed if time less than one hour
     } else {
-      fixed speed = stats.GetScoredSpeed();
+      // use overall speed if time less than one hour, else speed last hour
+      fixed speed = (stats.total.time_elapsed > fixed(3600) &&
+          !negative(stats.last_hour.duration)) ? stats.last_hour.speed : stats.GetScoredSpeed();
       stats.task_mc = UpdateTaskMCIfChanged(stats.task_mc, last_task_mc_speed,
                                             speed, glide_polar);
       glide_polar.SetMC(stats.task_mc);
