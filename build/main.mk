4e516d54dbfa449e597fa6d419b31120d8f12860
Ludovic Launer <ludovic.launer@gmail.com>
2017-11-07 15:04:30 -0500

    TH-755 Allow customisation of InfoBox titles through a file.
    XCSoarData/infoBoxTitle.locale

--- build/main.mk
+++ build/main.mk
@@ -413,6 +413,7 @@ XCSOAR_SOURCES := \
 	$(SRC)/InfoBoxes/InfoBoxWindow.cpp \
 	$(SRC)/InfoBoxes/InfoBoxLayout.cpp \
 	$(SRC)/InfoBoxes/InfoBoxManager.cpp \
+	$(SRC)/InfoBoxes/InfoBoxTitleLocale.cpp \
 	$(SRC)/InfoBoxes/Panel/Base.cpp \
 	$(SRC)/InfoBoxes/Panel/Altitude.cpp \
 	$(SRC)/InfoBoxes/Panel/AltitudeSimulator.cpp \
---
+++ src/InfoBoxes/InfoBoxTitleLocale.hpp
@@ -0,0 +1,45 @@
+/*
+Copyright_License {
+
+  Top Hat Soaring Glide Computer - http://www.tophatsoaring.org/
+  Copyright (C) 2000-2016 The Top Hat Soaring Project
+  A detailed list of copyright holders can be found in the file "AUTHORS".
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+}
+*/
+/*
+ * InfoBoxCustomTitle.hpp
+ *
+ *  Created on: Nov 3, 2017
+ *      Author: Ludovic Launer
+ */
+
+#ifndef SRC_INFOBOXES_INFOBOXTITLELOCALE_HPP_
+#define SRC_INFOBOXES_INFOBOXTITLELOCALE_HPP_
+
+#include "Profile/Map.hpp"
+
+namespace InfoBoxTitleLocale
+{
+  static ProfileMap mapTitleLocale;
+
+  bool Initialise();
+  bool LoadFile();
+  const char* GetLocale(const char *key);
+};
+
+
+#endif /* SRC_INFOBOXES_INFOBOXTITLELOCALE_HPP_ */
